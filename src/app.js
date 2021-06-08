fbe114f47ac0520959c912e37af9bf7bb744fb79
Hasan <hasan.doctype@gmail.com>
2021-06-08 12:30:07 +0400

    react starter v0.1

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/scss/bootstrap.scss';
+import './sass/style.scss';
+
+var temp = <h1>
+    hello react
+</h1>
+
+ReactDOM.render(temp, document.getElementById('root'));
